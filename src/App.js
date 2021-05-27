4071949f07663932ec32abdb9a3e30d95b5e33aa
zhangxiao625 <32870302+zhangxiao625@users.noreply.github.com>
2021-05-26 22:55:06 -0400

    Toggle reminder & conditional styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react' 
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    },
+  ])
+
+  const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
-       <h1>Hello World</h1>
+       <Header />
+       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) 
+       : ('No Tasks to Show')}
+       
     </div>
-  );
+  )
 }
 
 export default App;
