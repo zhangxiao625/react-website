9932bd13c342710f5f60f4c36483a22b9b62cbeb
zhangxiao625 <32870302+zhangxiao625@users.noreply.github.com>
2021-05-28 12:59:26 -0400

    added json server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import {useState} from 'react' 
+import AddTask from './components/AddTask'
+import {useState, useEffect} from 'react' 
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      "id": 1,
-      "text": "Doctors Appointment",
-      "day": "Feb 5th at 2:30pm",
-      "reminder": true
-    },
-    {
-      "id": 2,
-      "text": "Meeting at School",
-      "day": "Feb 6th at 1:30pm",
-      "reminder": true
-    },
-  ])
+  const [showAddTask, setShowAddTask] = useState(true)
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
 
   const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=>task.id === id ? {...task, reminder:
+      !task.reminder}:task))
+  }
+
   return (
     <div className="container">
-       <Header />
-       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) 
+       <Header 
+            onAdd={() => setShowAddTask
+            (!showAddTask)}
+            showAdd={showAddTask}
+       />
+       {showAddTask && <AddTask onAdd={addTask}/>}
+       {tasks.length > 0 ? (
+       <Tasks tasks={tasks} onDelete={deleteTask} 
+       onToggle={toggleReminder}/>) 
        : ('No Tasks to Show')}
        
     </div>
